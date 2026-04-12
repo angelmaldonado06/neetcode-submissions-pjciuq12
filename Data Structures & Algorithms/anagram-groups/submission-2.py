bdb1cbb579776940e5d34b7277de23c7d606b970
Angel Maldonado <afmaldonado6@gmail.com>
2026-04-12 12:15:45 -0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        '''
+        U
+        input: list of strings
+        output: list of lists of strings
+        group all anagrams together into these sublists
+        use dictionary
+
+        res = {
+            "ath" : ["hat"],
+            "act" : ["act", "cat"],
+            "opst" : ["stop", "pots", "tops"]
+        }
+
+        res["ath"] = ["hat"]
+
+        strs = ["act","pots","tops","cat","stop","hat"]
+
+        P
+        initialize empty dictionary
+        res = {}
+
+        iterate through the list of strings
+            if the sorted string is in dictionary:
+                append the word to the end of the list where the sorted word was found
+            else:
+                add the sorted word to the dictionary, and add a list as a value with the word
+
+        '''
+
+        res = {}
+
+        for word in strs:
+            if "".join(sorted(word)) in res:
+                res["".join(sorted(word))].append(word)
+            else:
+                res["".join(sorted(word))] = [word]
+
+        return [value for value in res.values()]
